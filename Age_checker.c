aee065a6787ef3fc14e0ede1542bcc5ebd74bfa6
ftmhanana2542-hub <ftmhanana2542@gmail.com>
2025-08-05 21:17:54 +0530

    Added age checker program

---
+++ Age_checker.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int age;
+    printf("Hi there! 👋 How old are you? ");
+scanf("%d", &age);
+if (age>=18)
+{
+  printf("✅ you're an adult now!Go and Chase your dream🚀");
+  }
+  else
+  {
+    printf("✨you're young explorer! keep learning and growing🌱");
+    }
+    return 0;
+    }
